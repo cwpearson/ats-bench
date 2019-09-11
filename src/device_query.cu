d3feff9da6d59d7b2395b6e598922158d574b9d1
Carl Pearson <pearson@illinois.edu>
2019-09-11 13:57:21 -0500

    add device-query binary

---
+++ src/device_query.cu
@@ -0,0 +1,92 @@
+/*!
+Adapted from Nvidia CUDA samples
+*/
+
+#include "common/check_cuda.cuh"
+#include "common/init.hpp"
+#include "common/logger.hpp"
+
+// Beginning of GPU Architecture definitions
+inline int _ConvertSMVer2Cores(int major, int minor) {
+  // Defines for GPU Architecture types (using the SM version to determine
+  // the # of cores per SM
+  typedef struct {
+    int SM; // 0xMm (hexidecimal notation), M = SM Major version,
+    // and m = SM minor version
+    int Cores;
+  } sSMtoCores;
+
+  sSMtoCores nGpuArchCoresPerSM[] = {
+      {0x30, 192}, {0x32, 192}, {0x35, 192}, {0x37, 192}, {0x50, 128},
+      {0x52, 128}, {0x53, 128}, {0x60, 64},  {0x61, 128}, {0x62, 128},
+      {0x70, 64},  {0x72, 64},  {0x75, 64},  {-1, -1}};
+
+  int index = 0;
+
+  while (nGpuArchCoresPerSM[index].SM != -1) {
+    if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor)) {
+      return nGpuArchCoresPerSM[index].Cores;
+    }
+
+    index++;
+  }
+
+  // If we don't find the values, we default use the previous one
+  // to run properly
+  printf("MapSMtoCores for SM %d.%d is undefined."
+         "  Default to use %d Cores/SM\n",
+         major, minor, nGpuArchCoresPerSM[index - 1].Cores);
+  return nGpuArchCoresPerSM[index - 1].Cores;
+}
+
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+
+  int deviceCount = 0;
+  CUDA_RUNTIME(cudaGetDeviceCount(&deviceCount));
+
+  // This function call returns 0 if there are no CUDA capable devices.
+  if (deviceCount == 0) {
+    printf("There are no available device(s) that support CUDA\n");
+  } else {
+    fmt::print("Detected {} CUDA Capable device(s)", deviceCount);
+  }
+
+  int dev, driverVersion = 0, runtimeVersion = 0;
+
+  for (dev = 0; dev < deviceCount; ++dev) {
+    CUDA_RUNTIME(cudaSetDevice(dev));
+    cudaDeviceProp deviceProp;
+    CUDA_RUNTIME(cudaGetDeviceProperties(&deviceProp, dev));
+
+    fmt::print("\nDevice {}: \"{}\"\n", dev, deviceProp.name);
+
+    // Console log
+    CUDA_RUNTIME(cudaDriverGetVersion(&driverVersion));
+    CUDA_RUNTIME(cudaRuntimeGetVersion(&runtimeVersion));
+    fmt::print(
+        "  CUDA Driver Version / Runtime Version          {}.{} / {}.{}\n",
+        driverVersion / 1000, (driverVersion % 100) / 10, runtimeVersion / 1000,
+        (runtimeVersion % 100) / 10);
+    fmt::print("  CUDA Capability Major/Minor version number:    {}.{}\n",
+               deviceProp.major, deviceProp.minor);
+
+    fmt::print("  Total amount of global memory:                 {} MBytes "
+               "({} bytes)\n",
+               static_cast<float>(deviceProp.totalGlobalMem / 1048576.0f),
+               (unsigned long long)deviceProp.totalGlobalMem);
+
+    fmt::print(
+        "  ({:3}) Multiprocessors, ({:3}) CUDA Cores/MP:    {} CUDA Cores\n",
+        deviceProp.multiProcessorCount,
+        _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor),
+        _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) *
+            deviceProp.multiProcessorCount);
+    fmt::print("  GPU Max Clock rate:                            {} MHz ({} "
+               "GHz)\n",
+               deviceProp.clockRate * 1e-3f, deviceProp.clockRate * 1e-6f);
+  }
+
+  exit(EXIT_SUCCESS);
+}
